36d7a160509f723975fa760f054116f921cbf79a
jphong <redwinemark@gmail.com>
2019-12-24 21:37:32 +0900

    Apply jpa auditing for posts domain object

--- src/main/java/com/follow/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/follow/book/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.follow.book.springboot.domain.posts;
 
+import com.follow.book.springboot.domain.common.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
