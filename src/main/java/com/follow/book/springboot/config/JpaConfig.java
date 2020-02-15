25de269c8978d21e9b7a4080c4f6fcff80a592a4
jphong <redwinemark@gmail.com>
2020-02-15 22:17:30 +0900

    Seperate @EnableJpaAuditing to make test cases pass

--- src/main/java/com/follow/book/springboot/config/JpaConfig.java
+++ src/main/java/com/follow/book/springboot/config/JpaConfig.java
@@ -1,7 +1,9 @@
 package com.follow.book.springboot.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@Configuration
 @EnableJpaAuditing
 public class JpaConfig {
 
